04899488b5a60b8a733aa9a2a5895295e1a46fdd
Esteban Flamini <38509691+estebanflamini@users.noreply.github.com>
2023-02-11 11:43:51 -0300

    Created a List class for browsing the list of items, plus some changes to expand_item() for the sake of consistency

--- someday.py
+++ someday.py
@@ -54,9 +54,48 @@ def get_items():
 
     return items, line_numbers
 
+# An utility class for showing a browsable list
+
+class List:
+    def __init__(self, items, screen, topy, topx, boty, botx):
+        self._items = items
+        self._screen = screen
+        self._topy = topy
+        self._topx = topx
+        self._height = boty - topy + 1
+        self._width = botx - topx + 1
+        self._first_item = 0
+        self._selected_row = 0
+
+    def show(self):
+        for i, item in enumerate(self._items[self._first_item:]):
+            if i >= self._height:
+                break
+            color = 2 if i == self._selected_row else 1
+            self._screen.addstr(self._topy + i, self._topx, item[:self._width], curses.color_pair(color))
+
+    def up(self):
+        if self._selected_row > 0:
+            self._selected_row -= 1
+        elif self._first_item > 0:
+            self._first_item -= 1
+
+    def down(self):
+        if self._selected_row < self._height - 1:
+            self._selected_row += 1
+        elif self._first_item + self._selected_row < len(self._items) - 1:
+            self._first_item += 1
+
+    def selected_item(self):
+        return self._first_item + self._selected_row
+
+    def selected_row(self):
+        return self._selected_row
+
 # This is the main function for browsing and updating the list of items
 
 def main(stdscr):
+
     # Initialize curses
     curses.curs_set(0)
 
@@ -76,15 +115,14 @@ def main(stdscr):
     # Get the list of items
     items, _ = get_items()
 
+    # Create an onscreen list for showing the items
+    item_list = List(items, stdscr, first_row, 0, last_row, width-1)
+
     # Define a menu of actions to be applied to the selected item
     menu = ["Action 1", "Action 2", "Action 3"]
 
-    # Initialize the selected item and action
-    selected_item = 0
+    # Initialize the selected action
     selected_action = 0
-
-    # Initialize the first shown item
-    first_item = 0
 
     # Main loop for handling key inputs
     while True:
@@ -99,15 +137,7 @@ def main(stdscr):
         if not items:
             stdscr.addstr(first_row, 0, "No items were found for today (and surrounding dates).")
         else:
-            row = first_row
-            last_item = -1
-            for i, item in enumerate(items[first_item:]):
-                if row > last_row:
-                    break
-                color = 2 if i == selected_item else 1
-                stdscr.addstr(row, 0, item[:width], curses.color_pair(color))
-                last_item = i
-                row += 1
+            item_list.show()
 
         # Draw the menu of actions
         for i, action in enumerate(menu):
@@ -121,37 +151,33 @@ def main(stdscr):
 
         # Handle the cursor keys to navigate the list of items and the menu of actions
         if key == curses.KEY_UP:
-            if selected_item > 0:
-                selected_item -= 1
-            elif first_item > 0:
-                first_item -= 1
+            item_list.up()
         elif key == curses.KEY_DOWN:
-            if selected_item < last_item:
-                selected_item += 1
-            elif first_item + selected_item < len(items) - 1:
-                first_item += 1
+            item_list.down()
         elif key == curses.KEY_LEFT and selected_action > 0:
             selected_action -= 1
         elif key == curses.KEY_RIGHT and selected_action < len(menu) - 1:
             selected_action += 1
         elif key == 10:
-            expand_item(items[first_item+selected_item], first_row + selected_item, last_row, width)
+            row = first_row + item_list.selected_row()
+            expand_item(items[item_list.selected_item()], row, 0, last_row, width-1)
         elif chr(key).lower() == "q":
             break
 
 def get_date():
     return subprocess.run(["when", "d"], capture_output=True).stdout
 
-def expand_item(item, row, last_row, width):
-    row -= 1
+def expand_item(item, topy, topx, boty, botx):
+    topy -= 1
+    width = botx - topx + 1
     lines = textwrap.wrap(item, width-2)
-    rows = len(lines) + 2
-    pad = curses.newpad(rows, width)
+    height = len(lines) + 2
+    pad = curses.newpad(height, width)
     pad.border()
     for i, line in enumerate(lines):
         pad.addstr(i+1, 1, line)
-    row = min(row, last_row - rows + 1)
-    pad.refresh(0, 0, row, 0, last_row, width)
+    topy = min(topy, boty - height + 1)
+    pad.refresh(0, 0, topy, topx, boty, botx)
     pad.getch()
 
 if __name__ == "__main__":
