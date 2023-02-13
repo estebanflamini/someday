d7ac7ea2e59cae92c8e296b85a27a62534a73d1d
Esteban Flamini <38509691+estebanflamini@users.noreply.github.com>
2023-02-13 16:52:48 -0300

    Improved separation of concerns between the Calendar singleton, the onscreen list of items and the main loop

--- someday.py
+++ someday.py
@@ -23,72 +23,74 @@ class Calendar:
             prefs = f.read()
         m = re.match(r"^\s*calendar\s*=\s*(.+)$", prefs, flags=re.MULTILINE)
         if m is not None:
-            self.calendar = m.group(1).strip()
+            self._calendar = m.group(1).strip()
         else:
             sys.exit("No calendar configuration for 'when' was found.")
 
 # TODO: Eliminar esta línea cuando ya estés seguro de que anda bien
-        self.calendar = "%s/prog/someday/calendar" % os.environ["HOME"]
+        self._calendar = "%s/prog/someday/calendar" % os.environ["HOME"]
 
-        self.proxy_calendar = self.calendar + ".SOMEDAY"
+        self._proxy_calendar = self._calendar + ".SOMEDAY"
 
-        with open(self.calendar) as infile:
-            self.calendar_lines = infile.read().splitlines()
+        with open(self._calendar) as infile:
+            self._calendar_lines = infile.read().splitlines()
 
-        self.line_numbers = []
-        self.modified = False
+        self._line_numbers = []
+        self._modified = False
 
     def check_no_proxy_calendar_exists(self):
-        if os.path.exists(self.proxy_calendar):
-            sys.exit("The calendar seems to be in edition. Delete the file %s and try again." % self.proxy_calendar)
+        if os.path.exists(self._proxy_calendar):
+            sys.exit("The calendar seems to be in edition. Delete the file %s and try again." % self._proxy_calendar)
 
     def cleanup_proxy_calendar(self):
-        os.unlink(self.proxy_calendar)
+        os.unlink(self._proxy_calendar)
 
-    # Copy the when's calendary to a temporary file where each non-empty line is line-numbered
+    # Copy the when's calendary to a temporary file where each non-empty line is line-numbered, and get upcoming items from there
 
     def generate_proxy_calendar(self):
         i = 0
-        with open(self.proxy_calendar, "w") as outfile:
-            for line in self.calendar_lines:
+        with open(self._proxy_calendar, "w") as outfile:
+            for line in self._calendar_lines:
                 tmp_line = "%s-%s" % (line, i) if line.strip() else line
                 print(tmp_line, file=outfile)
                 i += 1
 
-    # Use the temporary file created above as input for when to get a list of items along with their line numbers
-
-    def get_items(self):
-        tmp = subprocess.run(["when", "--calendar=%s" % self.proxy_calendar, "--noheader", "--wrap=0"],
+        tmp = subprocess.run(["when", "--calendar=%s" % self._proxy_calendar, "--noheader", "--wrap=0"],
                              capture_output=True, text=True, check=True).stdout
         tmp = re.findall(r"^(.+)-(\d+)$", tmp, flags=re.MULTILINE)
-        self.items = [x[0] for x in tmp]
-        self.line_numbers = [int(x[1]) for x in tmp]
-        self.modified = False
-        return self.items
+        self._items = [x[0] for x in tmp]
+        self._line_numbers = [int(x[1]) for x in tmp]
+
+    def no_items(self):
+        return not self._items
+
+    def get_items(self):
+        return self._items
 
     # Update the true calendar
 
     def write_calendar(self):
-        if self.modified:
-            copyfile(calendar, calendar + ".SOMEDAY.BAK")
-            with open(calendar, "w") as f:
-                for line in calendar_lines:
+        if self._modified:
+            copyfile(self._calendar, self._calendar + ".SOMEDAY.BAK")
+            with open(self._calendar, "w") as f:
+                for line in self._calendar_lines:
                     print(line, file=f)
 
-def erase(selected_item, minrow, mincol, maxrow, maxcol):
-    global modified
+    # Actions on the calendar
 
-    line_number = line_numbers[selected_item]
-    del calendar_lines[line_number]
-    del items[selected_item]
-    del line_numbers[selected_item]
-    modified = True
+    def erase(self, selected_item, minrow, mincol, maxrow, maxcol):
+        line_number = self._line_numbers[selected_item]
+        del self._calendar_lines[line_number]
+        self.generate_proxy_calendar()
+        self._modified = True
 
 Action = namedtuple("Action", ["key", "name", "action"])
 
-menu = [Action("e", "Erase", erase)]
-key_bindings = {x.key: x.action for x in menu}
-key_bindings[curses.KEY_DC] = erase
+def generate_menu(calendar):
+    menu = [Action("e", "Erase", calendar.erase)]
+    key_bindings = {x.key: x.action for x in menu}
+    key_bindings[curses.KEY_DC] = calendar.erase
+    return menu, key_bindings
 
 def expand_item(selected_item, minrow, mincol, maxrow, maxcol):
     minrow -= 1
@@ -107,11 +109,11 @@ def expand_item(selected_item, minrow, mincol, maxrow, maxcol):
 def get_date():
     return subprocess.run(["when", "d"], capture_output=True).stdout
 
-# An utility class for showing a browsable list
+# An utility class for browsing the calendar's items
 
 class List:
-    def __init__(self, items, screen, minrow, mincol, maxrow, maxcol):
-        self._items = items
+    def __init__(self, calendar, screen, minrow, mincol, maxrow, maxcol):
+        self._calendar = calendar
         self._screen = screen
         self._minrow = minrow
         self._mincol = mincol
@@ -121,7 +123,7 @@ class List:
         self._selected_row = 0
 
     def show(self):
-        for i, item in enumerate(self._items[self._first_item:]):
+        for i, item in enumerate(calendar.get_items()[self._first_item:]):
             if i >= self._height:
                 break
             color = 2 if i == self._selected_row else 1
@@ -165,10 +167,11 @@ def main(stdscr, calendar):
     last_row = height - 3
     menu_row = height - 1
 
-    items = calendar.get_items()
-
     # Create an onscreen list for showing the items
-    item_list = List(items, stdscr, first_row, 0, last_row, width-1)
+    item_list = List(calendar, stdscr, first_row, 0, last_row, width-1)
+
+    # Generate the menu and key bindings
+    menu, key_bindings = generate_menu(calendar)
 
     # Initialize the selected action
     selected_action = 0
@@ -183,7 +186,7 @@ def main(stdscr, calendar):
         stdscr.addstr(0, 0, get_date())
 
         # Draw the list of items
-        if not items:
+        if calendar.no_items():
             stdscr.addstr(first_row, 0, "No items were found for today (and surrounding dates).")
         else:
             item_list.show()
